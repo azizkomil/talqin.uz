ff50d30582e66c5eeaff5a22af78733a1148cf7a
azizkomil <komiljonovazizjon>
2024-05-05 16:06:46 +0500

    first push

---
+++ Talqin/script.js
@@ -0,0 +1,39 @@
+// Navbar START
+
+const nav = document.querySelector('.nav'),
+searchIcon = document.querySelector('#searchIcon'),
+navOpenBtn = document.querySelector('.navOpenBtn'),
+navCloseBtn = document.querySelector('.navCloseBtn')
+
+searchIcon.addEventListener('click', () => {
+    nav.classList.toggle('openSearch')
+    nav.classList.remove('openNav')
+    if (nav.classList.contains('openSearch')) {
+        return searchIcon.classList.replace('uil-search', 'uil-times')
+    }
+    searchIcon.classList.replace('uil-times', 'uil-search')
+})
+
+navOpenBtn.addEventListener('click', () => {
+    nav.classList.add('openNav')
+    nav.classList.remove('openSearch')
+    searchIcon.classList.replace('uil-times', 'uil-search')
+})
+navCloseBtn.addEventListener('click', () => {
+    nav.classList.remove('openNav')
+})
+
+// Navbar END
+
+// News wrap START
+
+$(document).ready(function () {
+    $("#news-slider").owlCarousel({
+        items: 3,
+        navigation: true,
+        navigationText: ["", ""],
+        autoPlay: true,
+    });
+});
+
+// News wrap END
